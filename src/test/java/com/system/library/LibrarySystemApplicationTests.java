de92c1311f68fe1374de665e62664e65a151f873
zwundzwzig <amoeba8282@gmail.com>
2023-11-19 01:06:27 +0900

    초기 세팅

---
+++ src/test/java/com/system/library/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
